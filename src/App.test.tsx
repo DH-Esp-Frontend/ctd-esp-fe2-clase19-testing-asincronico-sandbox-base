3c5586774801b3f80ec81b7005ae2c34402133c5
Sabrina Islas <sabri.islas96@gmail.com>
2022-05-25 18:58:10 -0300

    Completed exercise

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import {render, screen, waitFor, waitForElementToBeRemoved} from "@testing-library/react";
 import App from "./App";
 
 jest.mock("axios");
@@ -20,37 +19,16 @@ const fakeUsers = [
 describe("App", () => {
   describe("Cuando finaliza la carga", () => {
     test("No debe mostrar el cargando", async () => {
-      axios.get.mockResolvedValueOnce({ data: fakeUsers });
 
-      render(<App />);
-
-      await waitForElementToBeRemoved(screen.getByText("Cargando usuarios..."))
-      await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
     });
     test("Debe mostrar el titulo de Usuarios", async () => {
-      axios.get.mockResolvedValueOnce({ data: fakeUsers });
-      render(<App />);
-      await waitForElementToBeRemoved(screen.getByText("Cargando usuarios..."))
 
-      expect(await screen.findByText("Usuarios:")).toBeInTheDocument();
     });
     test("Debe mostrar el username", async () => {
-      axios.get.mockResolvedValueOnce({ data: fakeUsers });
 
-      render(<App />);
-
-      const userList = await screen.findByText("@stevesantos");
-
-      expect(userList).toBeInTheDocument();
     });
     test("Debe mostrar el nombre del usuario", async () => {
-      axios.get.mockResolvedValueOnce({ data: fakeUsers });
 
-      render(<App />);
-
-      const userList = await screen.findByText("Steve");
-
-      expect(userList).toBeInTheDocument();
     });
   });
 });
